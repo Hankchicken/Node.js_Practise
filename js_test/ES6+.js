86a3852640e5ab96a5ad1eabf8f6813403066ce6
Hankchicken <damond891207@gmail.com>
2025-08-12 22:20:00 +0800

    20250812

--- js_test/ES6+.js
+++ js_test/ES6+.js
@@ -1,4 +1,3 @@
 const product = { id: 101, name: "電競滑鼠", price: 1290 };
 const { name, price } = product;
-console.log("產品名稱: ", name);
-console.log("售價: ", price)
+console.log(`產品名稱: ${name}, 售價: ${price}`);
