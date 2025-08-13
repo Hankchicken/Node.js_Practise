b4cf6b7df2c192e0bc42fbafcd4349b8b1e80efd
Hankchicken <cbyuan891207@gmail.com>
2025-08-13 11:16:07 +0800

    20250813

--- js_test/array.js
+++ js_test/array.js
@@ -3,4 +3,11 @@ const doubled = number.map(num => num * 2);
 console.log('雙數:', doubled);
 
 
+const fruits = ['蘋果', '香蕉', '橘子'];
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+for (const fruit of fruits) {
+    console.log(fruits);
+}
 
--- js_test/fruits.js
+++
@@ -1,9 +0,0 @@
-const fruits = ['蘋果', '香蕉', '橘子'];
-
-for (let i = 0; i < fruits.length; i++) {
-    console.log(fruits[i]);
-}
-
-for (const fruit of fruits) {
-    console.log(fruits);
-}
